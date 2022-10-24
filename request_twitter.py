60996d5b2d84269a7b61d10505b20500954aa612
João Paulo <joaopauloalbuquerque96@gmail.com>
2022-10-24 08:09:10 -0300

    Implementando busca de tweets por filtros.

--- request_twitter.py
+++ request_twitter.py
@@ -1,5 +1,6 @@
 import tweepy
 from credentials import *
+import json
 
 
 class AllStream(tweepy.StreamingClient):
@@ -13,22 +14,45 @@ class AllStream(tweepy.StreamingClient):
 
     def on_data(self, raw_data):
 
-        print(raw_data)
+        
+        json_response = json.loads(raw_data)
 
-    def on_status(self, status):
+        for key, value in json_response.items():
+            print(key)
+        
+        # if 'location' in json_response.keys():
+        #     tweet_data = [str(json_response['data']['id']),
+        #               str(json_response['data']['text']),
+        #               str(json_response['includes']['users'][0]['location'])]
 
-        print ("Tweet Text: ",status.text)
+        # print(tweet_data)
+
+    
+class StartStream():
+
+    def __init__(self, stream):
+        
+        self.stream = stream
+
+    def insert_filter(self, *filter):
+
+        for key in filter:
+            self.stream.add_rules(tweepy.StreamRule(key))
+
+    def start_stream(self):
+        self.stream.filter(expansions = "author_id", user_fields = ["location"] )
+
+
+stream = AllStream(API_BEARER_TOKEN)
+    
+stream_data = StartStream(stream)
+
+stream_data.insert_filter(["Lula"])
+
+stream_data.start_stream()
 
 
 
-stream_data = AllStream(API_BEARER_TOKEN)
-
-filter = ["Lula", "Bolsonaro"]
-
-for key in filter:
-    stream_data.add_rules(tweepy.StreamRule(key))
-
-stream_data.filter(tweet_fields=["referenced_tweets"])
     
 
 
